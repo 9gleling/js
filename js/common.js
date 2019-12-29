bca2ad279f54a14b9c36353a4e20988551caf1c1
9gleling <9gleling@gmail.com>
2019-12-29 15:30:13 +0900

    add common header footer

---
+++ js/common.js
@@ -0,0 +1,8 @@
+
+$(document).ready( function() {
+
+    $("#sidebar-wrapper").load("common/side.html");
+    $("#header").load("common/header.html");
+    $("#footer").load("common/footer.html");
+});
+
